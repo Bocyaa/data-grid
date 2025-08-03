e5132c3e24ca20688dfbf249970fc9f4ec5cf41e
Abdubositkhon Khabirov <xabirovbob@gmail.com>
2025-08-03 17:33:40 +0200

    Configure frontend for deployment

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -1,4 +1,7 @@
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || 'http://localhost:5174/api';
+  import.meta.env.VITE_API_BASE_URL ||
+  (import.meta.env.MODE === 'production'
+    ? 'https://data-grid.railway.internal/api'
+    : 'http://localhost:5174/api');
 
 export { API_BASE_URL };
